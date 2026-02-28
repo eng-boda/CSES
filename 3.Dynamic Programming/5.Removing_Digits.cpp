ab04c3772ee4409eecf4089e781420e749844858
Abdelrahman Ahmed <abdelrahman6as@gmail.com>
2026-03-01 00:23:36 +0200

    Create 5.Removing_Digits.cpp

---
+++ 3.Dynamic Programming/5.Removing_Digits.cpp
@@ -0,0 +1,45 @@
+/*
+  Problem Name: Removing Digits
+  Problem Link: cses.fi/problemset/task/1637/
+  Author: EngBoda
+  Time Complexity: O(n)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define BODA ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0);
+#define all(x) x.begin() , x.end()
+#define YES cout << "YES" << '\n'
+#define NO cout << "NO" << '\n'
+#define MOD 1000000007
+#define INF 1e18
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+char dir[] = {'U', 'D', 'L', 'R'};
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> dp(n+1 , 1e9);
+    dp[0] = 0;
+    for (int i=1;i<=n;i++) {
+        int temp = i;
+        while (temp) {
+            int digit = temp%10;
+            dp[i] = min(dp[i], 1 + dp[i - digit]);
+            temp = temp/10;
+        }
+    }
+    cout << dp[n] << '\n';
+}
+ 
+int main() {
+    // Welcome to Boda's world
+    BODA;
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
