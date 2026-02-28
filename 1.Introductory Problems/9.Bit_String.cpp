960508626e8fab919337141e4def522449178827
Abdelrahman Ahmed <abdelrahman6as@gmail.com>
2026-02-28 23:06:23 +0200

    Create 9.Bit_String.cpp

---
+++ 1.Introductory Problems/9.Bit_String.cpp
@@ -0,0 +1,39 @@
+/*
+  Problem Name: Bit String
+  Problem Link: cses.fi/problemset/task/1617/
+  Author: EngBoda
+  Time Complexity: O(n)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define BODA ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0);
+#define all(x) x.begin() , x.end()
+#define YES cout << "YES" << '\n'
+#define NO cout << "NO" << '\n'
+#define MOD 1000000007
+#define INF 1e18
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+char dir[] = {'U', 'D', 'L', 'R'};
+
+void solve() {
+    int n;
+    cin >> n;
+    ll ans = 1;
+    for (int i=1;i<=n;i++) {
+        ans = (ans * 2) % MOD;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    // Welcome to Boda's world
+    BODA;
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
